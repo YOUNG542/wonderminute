d69381f65c340e949d3c5353f86e400563331dec
startalk <yd030818@naver.com>
2025-08-02 09:51:06 +0900

    Initial Commit

---
+++ wonderminute/wonderminuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  wonderminuteApp.swift
+//  wonderminute
+//
+//  Created by 김영대 on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct wonderminuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
